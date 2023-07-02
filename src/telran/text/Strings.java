07396d59663be8401e6e4796afd48237545dec2c
Natali Shtofmakher <natalilg2010@gmail.com>
2023-07-02 18:06:04 +0300

    HW#25

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -1,28 +1,90 @@
 package telran.text;
 
+import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.function.BinaryOperator;
 
 public class Strings {
 
-	public static String javaVariableName() {
-
-		return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	static HashMap<String, BinaryOperator<Double>> mapOperations;
+	static {
+		mapOperations = new HashMap<>();
+		mapOperations.put("-", (a, b) -> a - b);
+		mapOperations.put("+", (a, b) -> a + b);
+		mapOperations.put("*", (a, b) -> a * b);
+		mapOperations.put("/", (a, b) -> a / b);
 	}
-
-	public static String zero_300() {
-
-		return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+public static String javaVariableName() {
+	
+	return "([a-zA-Z$][\\w$]*|_[\\w$]+)";
+}
+public static String zero_300() {
+	
+	return "[1-9]\\d?|[1-2]\\d\\d|300|0";
+}
+public static String ipV4Octet() {
+	//TODO
+	//positive number from 0 to 255 and leading zeros are allowed
+	return  "([01]?\\d\\d?|2([0-4]\\d|5[0-5]))";
+}
+public static String ipV4() {
+	String octetRegex = ipV4Octet();
+	return String.format("(%s\\.){3}%1$s",octetRegex);
+}
+public static String arithmeticExpression() {
+	String operandRE = operand();
+	String operatorRE = operator();
+	return String.format("%1$s(%2$s%1$s)*",operandRE, operatorRE );
+}
+public static String operator() {
+	return "\\s*([-+*/])\\s*";
+}
+private static String operand() {
+	String numberExp = numberExp();
+	String variableExp = javaVariableName();
+	return String.format("((%s|%s))", numberExp, variableExp);
+}
+private static String numberExp() {
+	return "(\\d+\\.?\\d*|\\.\\d+)";
+}
+public static boolean isArithmeticExpression(String expression) {
+	expression = expression.trim();
+	return expression.matches(arithmeticExpression());
+}
+public static double computeExpression(String expression,
+		HashMap<String, Double> mapVariables) {
+	
+	if (!isArithmeticExpression(expression)) {
+		throw new IllegalArgumentException("Wrong arithmetic expression");
 	}
-
-	public static String ipV4Octet() {
-		
-		return "[01]?\\d\\d?|2([0-4]\\d|5[0-5])";
-
+	expression = expression.replaceAll("\\s+", "");
+	String[] operands = expression.split(operator());
+	String [] operators = expression.split(operand());
+	double res = getValue(operands[0], mapVariables);
+	for(int i = 1; i < operands.length; i++) {
+		double operand = getValue(operands[i], mapVariables);
+		res = mapOperations.get(operators[i]).apply(res, operand);
 	}
-
-	public static String ipV4() {
-		String octetRegex = ipV4Octet();
-		return String.format("((%s))[.]){3}(%s)", octetRegex, octetRegex);
+	
+	return res;
+}
+private static double getValue(String operand, HashMap<String, Double> mapVariables) {
+	double res = 0;
+	if (operand.matches(javaVariableName())) {
+		if(!mapVariables.containsKey(operand)) {
+			throw new NoSuchElementException();
+		}
+		res = mapVariables.get(operand);
+	} else {
+		res = Double.parseDouble(operand);
 	}
+	return res;
+}
 
+	
+	
+	
+	
+	
 }
---
+++ src/telran/text/test/StringsTest.java
@@ -0,0 +1,126 @@
+package telran.text.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import telran.text.Strings;
+
+class StringsTest {
+
+	static HashMap<String, Double> mapVariables;
+	static {
+		mapVariables = new HashMap<>();
+		mapVariables.put("abc", 6.0);
+		mapVariables.put("a1", 1000.0);
+		mapVariables.put("a2", 60.0);
+	}
+		
+			
+			
+		
+		@Test
+		void javaVariableNameTest() {
+			String regex = Strings.javaVariableName();
+			assertTrue("a".matches(regex));
+			assertTrue("$".matches(regex));
+			assertTrue("$2".matches(regex));
+			assertTrue("$_".matches(regex));
+			assertTrue("__".matches(regex));
+			assertTrue("_2".matches(regex));
+			assertTrue("a_b".matches(regex));
+			assertTrue("A_B".matches(regex));
+			assertTrue("abc12345678900000".matches(regex));
+			assertFalse("1a".matches(regex));
+			assertFalse("_".matches(regex));
+			assertFalse("a#".matches(regex));
+			assertFalse("a b".matches(regex));
+			assertFalse("a-b".matches(regex));
+			assertFalse(" ab".matches(regex));
+			
+		}
+		@Test
+		void zero_300Test() {
+			String regex = Strings.zero_300();
+			assertTrue("0".matches(regex));
+			assertTrue("9".matches(regex));
+			assertTrue("299".matches(regex));
+			assertTrue("300".matches(regex));
+			assertTrue("99".matches(regex));
+			assertFalse("01".matches(regex));
+			assertFalse("00".matches(regex));
+			assertFalse("1111".matches(regex));
+			assertFalse("301".matches(regex));
+			assertFalse("3000".matches(regex));
+			assertFalse("310".matches(regex));
+			assertFalse("-1".matches(regex));
+			assertFalse("3 ".matches(regex));
+			
+			
+			
+		}
+		@Test
+		void ipV4OctetTest() {
+			String regex = Strings.ipV4Octet();
+			assertTrue("000".matches(regex));
+			assertTrue("00".matches(regex));
+			assertTrue("0".matches(regex));
+			assertTrue("99".matches(regex));
+			assertTrue("1".matches(regex));
+			assertTrue("10".matches(regex));
+			assertTrue("199".matches(regex));
+			assertTrue("200".matches(regex));
+			assertTrue("249".matches(regex));
+			assertTrue("250".matches(regex));
+			assertTrue("255".matches(regex));
+			assertFalse("0000".matches(regex));
+			assertFalse(" 1".matches(regex));
+			assertFalse(".0".matches(regex));
+			assertFalse("-1".matches(regex));
+			assertFalse("256".matches(regex));
+			assertFalse("1000".matches(regex));
+		}
+		@Test
+		void IpV4Test() {
+			String regex = Strings.ipV4();
+			assertTrue("0.0.0.0".matches(regex));
+			assertTrue("1.1.1.1".matches(regex));
+			assertTrue("99.99.12.09".matches(regex));
+			assertTrue("100.199.200.255".matches(regex));
+			assertFalse(".1.2.3.4".matches(regex));
+			assertFalse("1.2.3.4.".matches(regex));
+			assertFalse(".1.&2.3.4".matches(regex));
+			assertFalse("1.2.3".matches(regex));
+			assertFalse("1.2.3.4.5".matches(regex));
+			assertFalse("123 123 123 123".matches(regex));
+		}
+		@Test
+		void arithmeticExpressionTest() {
+			assertTrue(Strings.isArithmeticExpression(" 12 "));//12
+			assertTrue(Strings.isArithmeticExpression(" 12/ abc"));//abc = 6
+			assertTrue(Strings.isArithmeticExpression("12/2"));//6
+			assertTrue(Strings.isArithmeticExpression(" 12*  2 / 3 + a1 "));//a1 = 1000
+			assertTrue(Strings.isArithmeticExpression(" 120 / a2 + 100 - 2 * 2.5 / 500 "));//a2=60
+			assertFalse(Strings.isArithmeticExpression(" 12 18"));
+			assertFalse(Strings.isArithmeticExpression(" 12/3&4"));
+			assertFalse(Strings.isArithmeticExpression(" 12+20-"));
+			assertFalse(Strings.isArithmeticExpression(" 12/ 18 + 100 10"));
+			assertFalse(Strings.isArithmeticExpression(" 12/ 18 + 100 + 1v"));
+			
+		}
+		@Test
+		void computeExpressionTest() {
+			assertEquals(12, Strings.computeExpression(" 12 ", mapVariables));
+			assertEquals(2, Strings.computeExpression(" 12/ 6", mapVariables));
+			assertEquals(6, Strings.computeExpression("12/2", mapVariables));
+			assertEquals(1008, Strings.computeExpression(" 12*  2 / 3 + 1000 ", mapVariables));
+			assertEquals(0.5, Strings.computeExpression(" 120 / a2 + 100 - 2 * 2.5 / 500 ", mapVariables));
+			assertThrowsExactly(IllegalArgumentException.class,
+					() -> Strings.computeExpression(" 12/ 18 + 100 10", mapVariables));
+			assertThrowsExactly(NoSuchElementException.class, () ->
+			Strings.computeExpression(" 12/ 18 + 100-a15", mapVariables));
+		}
+}
